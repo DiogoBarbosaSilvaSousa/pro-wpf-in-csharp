7f797a7d9d609375a7c741a5a74d8d4cda817899
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-11-07 12:51:22 -0300

    Acrescentada correção.

---
+++ parte-1-fundamentos/capitulo-5-eventos-roteados/BubbledLabelClick/BubbledLabelClick/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BubbledLabelClick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected int eventCounter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SomethingClicked(object sender, MouseButtonEventArgs e)
+        {
+            eventCounter++;
+            string message = "#" + eventCounter.ToString() + ":\r\n" +
+            " Sender(Remetente): " + sender.ToString() + "\r\n" +
+            " Source(Fonte): " + e.Source + "\r\n" +
+            " Original Source(Fonte original): " + e.OriginalSource;
+            lstMessages.Items.Add(message);
+            e.Handled = (bool)chkHandle.IsChecked;
+
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e)
+        {
+            eventCounter = 0;
+            lstMessages.Items.Clear();
+        }
+    }
+}
